2f4812978e8ef2197dee3d9202ff51e2753f51b2
anushikhov <anushikhov@users.noreply.github.com>
2021-09-19 22:47:37 +0400

    R installation

---
+++ r_installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update -y
+# You only need r-base-dev if you want to compile R packages yourself or software depending on R
+sudo apt-get install r-base r-base-dev -y
+
+# To install the automatically tuned Atlas or the multi-threaded OpenBlas library in order to get higher performancefor linear algebra operations.
+sudo apt-get install libatlas3-base -y
+
+# or 
+# sudo apt-get install libopenblas-base
+
+# https://cran.r-project.org/
+
